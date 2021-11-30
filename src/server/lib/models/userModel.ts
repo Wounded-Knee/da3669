0ccd23a8a8e0e80af950597799c491158273f92c
Joel Kramer <joel@nuffsaid.com>
2021-11-30 11:32:21 -0800

    Streamlining the mongoose schema management on the server

--- src/server/lib/models/userModel.ts
+++
@@ -1,9 +0,0 @@
-import NodeModel from './nodeModel';
-const { extend } = NodeModel;
-
-export default extend({
-  name: 'User',
-  schemaPaths: {
-    name: { type: String, required: true },
-  },
-});
