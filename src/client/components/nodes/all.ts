b6a4216035064348577436e423daa637344bb3d8
Joel Kramer <joel@nuffsaid.com>
2021-12-03 13:45:34 -0800

    Client-side node component loader

--- src/client/components/nodes/all.ts
+++ src/client/components/nodes/all.ts
@@ -1,22 +1,31 @@
 import { nodeTypes as sharedNodeTypes } from '../../../shared/config';
-const fileContents = {
-  Node: require('./Node').default,
-  Document: require('./Document').default,
-  Message: require('./Message').default,
+import Base from './Base';
+import Document from './Document';
+import Message from './Message';
+import User from './User';
+
+const componentData = {
+  [sharedNodeTypes[0]]: Base,
+  [sharedNodeTypes[1]]: Document,
+  [sharedNodeTypes[2]]: User,
+  [sharedNodeTypes[3]]: Message,
 };
 
-const nodeTypes = sharedNodeTypes.reduce((acc, nodeType) => {
-  const filename = `./${nodeType.name}`;
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const contents = fileContents[nodeType.name];
+const nodeTypes = sharedNodeTypes.reduce((acc, nodeTypeName) => {
+  const filename = `./${nodeTypeName}`;
   const definition = {
-    ...nodeType,
+    name: nodeTypeName,
     filename,
-    ...contents,
+    ...componentData[nodeTypeName],
   };
   return [...acc, definition];
 }, []);
 
 export const defaultNodeType = nodeTypes.find((nodeType) => nodeType.default === true);
 export const getNodeTypeByNodeData = ({ kind }) => getComponentByType(kind);
-export const getComponentByType = (type) => (nodeTypes.find(({ name }) => name === type) || defaultNodeType).Component;
+export const getComponentByType = (type) => {
+  const x = nodeTypes.find(({ name }) => name === type);
+  const y = x || defaultNodeType;
+  const z = y.Component;
+  return z;
+};
