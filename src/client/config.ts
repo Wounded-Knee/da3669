fbe1a508c537d9bf8d48857e53596578bba4bdb9
Joel Kramer <joel@nuffsaid.com>
2021-12-06 12:27:35 -0800

    Improved panic room

--- src/client/config.ts
+++ src/client/config.ts
@@ -17,23 +17,24 @@ import Atmosphere from './components/Atmosphere';
 import SavingsIcon from '@mui/icons-material/Savings';
 import { Bank } from './components/Banksy';
 import { Index as ContextStacker } from './wireframes/context-stacking/Index';
+import { Loading } from './components/Loading';
 
 const localState = get(appName) || { ui: { drawers: {} } };
 
 export const routes = [
+  {
+    route: '/panic',
+    path: '/panic',
+    icon: PanicIcon,
+    text: 'Panic',
+    component: Loading,
+  },
   {
     route: '/context',
     path: '/context',
     icon: ContextStackerIcon,
     text: 'Context Stacker',
     component: ContextStacker,
-  },
-  {
-    route: '/chat',
-    path: '/chat',
-    icon: PanicIcon,
-    text: 'Panic',
-    component: Chat,
   },
   {
     path: '/nodemanager/:nodeId',
