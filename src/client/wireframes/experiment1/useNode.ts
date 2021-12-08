178f4ad95d6d6afa73e4bad00285985c69c356af
Joel Kramer <joel@nuffsaid.com>
2021-12-08 13:51:15 -0800

    Better usage of the store cache.

--- src/client/wireframes/experiment1/useNode.ts
+++ src/client/wireframes/experiment1/useNode.ts
@@ -3,14 +3,14 @@ import { action } from './webSocket';
 import { server } from '../../../shared/lib/redux/actionTypes';
 import { store } from '../../lib/redux/store';
 import { useOnMount } from '../../lib/useOnMount';
+import { getNodeById } from './selectors';
 
 export const useNode = (id) => {
-  const [node, setNode] = useState();
+  const [node, setNode] = useState(getNodeById(store.getState(), id));
 
   useOnMount(() => {
     return store.subscribe(() => {
-      const state = store.getState();
-      setNode(state.nodes.find(({ _id }) => _id === id));
+      setNode(getNodeById(store.getState(), id));
     });
   });
 
