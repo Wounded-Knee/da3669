1c0e1be469008a32a5a7cc8a54783796cfc22521
Joel Kramer <joel@nuffsaid.com>
2021-12-12 01:00:25 -0800

    Kung Fury

--- src/client/components/KungFury.tsx
+++ src/client/components/KungFury.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
-import ReactPlayer from 'react-player';
+import ReactPlayer from 'react-player/dailymotion';
 
 const url = 'https://www.dailymotion.com/video/x2s5rnp';
 export const KungFury = () => {
-  return <ReactPlayer url={url} />;
+  return (
+    <ReactPlayer
+      url={url}
+      config={{
+        fullscreen: true,
+        autoplay: true,
+      }}
+      width='100%'
+      playing={true}
+    />
+  );
 };
