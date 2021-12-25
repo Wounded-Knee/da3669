1178c23be93bbd875a84f36680f8346e74bfa84f
Wakinyan (Heyoka) <jpkramer707_github@mm.st>
2021-12-25 10:53:53 -0800

    Context Stacker Refactor

--- src/client/wireframes/context-stacking/ContextStacker.tsx
+++ src/client/wireframes/context-stacking/ContextStacker.tsx
@@ -1,113 +1,31 @@
 /** @jsxFrag React.Fragment */
 /** @jsx jsx */
-import React from 'react';
+import React, { useEffect } from 'react';
 import { css, jsx } from '@emotion/react';
+import { useOnMount } from '../../lib/useOnMount';
 
-const reducer = (state, { type, payload }) => {
-  switch (type) {
-    case 'SET_TITLE':
-      return {
-        ...state,
-        title: payload,
-      };
+export const ContextStacker = (props) => {
+  const { child, ancestorProps, changeAncestorProps = (props) => props, ...forwardProps } = props;
+  const { depthLimit, depth = 0, text, color, callback } = props;
+  const descendantProps = changeAncestorProps(ancestorProps) || {};
+  const finalDescendant = !child || depth >= depthLimit;
 
-    case 'SET_TITLE_COLOR':
-      return {
-        ...state,
-        titleColor: payload,
-      };
-
-    case 'SET_TEXT':
-      return {
-        ...state,
-        text: payload,
-      };
-
-    case 'SET_HTML':
-      const Wrapper = payload;
-      const OldWrapper = state.html;
-      const NewHTML = ({ children }) => (
-        <Wrapper>
-          <OldWrapper>{children}</OldWrapper>
-        </Wrapper>
-      );
-      return {
-        ...state,
-        html: ({ children }) => <NewHTML>{children}</NewHTML>,
-      };
-
-    case 'CHAT':
-      return {
-        ...state,
-        chatLog: [...state.chatLog, payload],
-      };
-  }
-  return state;
-};
-
-export const ContextStacker = ({ state }) => {
-  let newState = { ...state, recursionIndex: state.recursionIndex + 1 };
-  switch (state.recursionIndex) {
-    case 3:
-      newState = reducer(newState, { type: 'SET_TITLE', payload: 'Hello, world!' });
-      break;
-    case 4:
-      newState = reducer(newState, { type: 'CHAT', payload: 'Hahaha!' });
-      break;
-    case 6:
-      newState = reducer(newState, { type: 'CHAT', payload: 'This is cool!' });
-      break;
-    case 9:
-      var html = (props) => <div id='outer'>{props.children}</div>;
-      newState = reducer(newState, { type: 'CHAT', payload: 'Yep!' });
-      newState = reducer(newState, { type: 'SET_HTML', payload: html });
-      break;
-    case 12:
-      var html = (props) => (
-        <div id='oopser'>
-          {text}
-          {props.children}
-        </div>
-      );
-      newState = reducer(newState, { type: 'SET_HTML', payload: html });
-      newState = reducer(newState, { type: 'SET_TITLE_COLOR', payload: 'red' });
-      break;
-    case 15:
-      newState = reducer(newState, { type: 'SET_TITLE_COLOR', payload: 'orange' });
-      break;
-    case 16:
-      newState = reducer(newState, { type: 'SET_TITLE_COLOR', payload: 'yellow' });
-      break;
-    case 17:
-      newState = reducer(newState, { type: 'SET_TITLE_COLOR', payload: 'green' });
-      break;
-    case 18:
-      newState = reducer(newState, { type: 'SET_TITLE_COLOR', payload: 'cyan' });
-      break;
-    case 23:
-      newState = reducer(newState, { type: 'SET_TITLE', payload: 'Reduced Context' });
-      break;
-  }
-  // @ts-ignore
-  const { title, html: Html, text, titleColor, recursionIndex, chatLog, recursionLimit } = newState;
+  useEffect(() => {
+    if (finalDescendant) {
+      console.log('callback');
+      callback(descendantProps);
+    }
+  }, [finalDescendant]);
 
   return (
     <>
-      {recursionIndex < recursionLimit ? (
-        <ContextStacker key={recursionIndex} state={newState} />
+      <p style={{ color }}>
+        {depth}: {text}
+      </p>
+      {!finalDescendant ? (
+        React.cloneElement(child, { ...forwardProps, ...child.props, ancestorProps: descendantProps, depth: depth + 1 })
       ) : (
-        <Html>
-          <h1
-            css={css`
-              color: ${titleColor || 'white'};
-            `}
-          >
-            {title}
-          </h1>
-          {chatLog.map((line, index) => (
-            <p key={index}>{line}</p>
-          ))}
-        </Html>
+        <></>
       )}
     </>
   );
--- src/client/wireframes/context-stacking/Index.tsx
+++ src/client/wireframes/context-stacking/Index.tsx
@@ -1,20 +1,27 @@
+/** @jsxFrag React.Fragment */
+/** @jsx jsx */
+import { css, jsx } from '@emotion/react';
 import React, { useState } from 'react';
 import { ContextStacker } from './ContextStacker';
 import { Slider } from '@mui/material';
-import { getTopLevelNodes } from '../experiment1/data';
-
-export const initialState = {
-  text: 'Untitled',
-  html: (props) => <>{props.children}</>,
-  chatLog: [],
-  recursionIndex: 0,
-  recursionLimit: 50,
-};
 
 export const Index = () => {
   const [depth, setDepth] = useState(0);
+  const [coolState, setCoolState] = useState({});
+
   return (
-    <>
+    <div
+      id='contextStacker'
+      css={css`
+        flex-grow: 1;
+        height: 100%;
+        margin: -1em;
+        overflow: hidden;
+        padding: 2em;
+        background-image: url('${coolState.background}');
+        transition: background-image 2s ease-in-out;
+      `}
+    >
       <Slider
         aria-label='Depth'
         defaultValue={0}
@@ -26,15 +33,40 @@ export const Index = () => {
         step={1}
         marks
         min={0}
-        max={50}
+        max={2}
       />
 
+      <h1>{coolState.title}</h1>
+
       <ContextStacker
-        state={{
-          ...initialState,
-          recursionLimit: depth,
+        text='Hello.'
+        depthLimit={depth}
+        ancestorProps={{
+          title: 'Welcome!',
         }}
+        callback={setCoolState}
+        color='cyan'
+        child={
+          <ContextStacker
+            text='Hey look I can change the background.'
+            color='yellow'
+            changeAncestorProps={(props) => ({
+              ...props,
+              background: 'https://i.pinimg.com/originals/66/f8/59/66f859fc32c72e5f8401cc03e09ebc18.png',
+            })}
+            child={
+              <ContextStacker
+                text='I would rather talk about trees.'
+                changeAncestorProps={(props) => ({
+                  ...props,
+                  title: 'Tree Chat',
+                  background: 'http://wallpaperose.com/wp-content/uploads/2013/07/Natural-Leaves-Widescreen.jpg',
+                })}
+              />
+            }
+          />
+        }
       />
-    </>
+    </div>
   );
 };
