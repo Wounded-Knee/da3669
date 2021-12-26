8c3da422bacabf05de14f8f7e18cd29a15565d93
Joel Kramer <joel@nuffsaid.com>
2021-12-25 18:39:42 -0800

    Context Stacker

--- src/client/wireframes/context-stacking/Index.tsx
+++ src/client/wireframes/context-stacking/Index.tsx
@@ -15,18 +15,63 @@ export const Index = () => {
   const [coolState, setCoolState] = useState({});
   const [path, setPath] = useState([]);
 
-  const addPath = (index) => {
-    console.log(index);
-    setPath([...path, index]);
-  };
+  const addPath = (index) => setPath([...path, index]);
+  const setDepthOk = (depth) => setPath(path.slice(0, depth));
+  useEffect(() => setDepth(path.length), [path.length]);
 
-  const setDepthOk = (depth) => {
-    setPath(path.slice(0, depth));
+  const data = {
+    addPath,
+    setDepth: setDepthOk,
+    path: path.slice(0, depth),
+    text: 'Hello.',
+    depthLimit: depth,
+    ancestorProps: {
+      title: 'Welcome!',
+    },
+    callback: setCoolState,
+    children: [
+      {
+        text: 'Blah blah blah, but, in cyan',
+        color: 'cyan',
+      },
+      {
+        text: 'I like camping.',
+        mutation: { background: 'http://jpkramer.com/fire2.gif' },
+      },
+      {
+        text: 'Who wants to talk spaceships?',
+        mutation: {
+          title: 'Spaceship Talk!',
+          background: 'https://blogs.esa.int/alexander-gerst/files/2014/03/10729802334_f51e9b69d8_o.jpg',
+        },
+      },
+      { text: 'Britta, are you here?' },
+      {
+        text: 'Hey look I can change the background.',
+        color: 'yellow',
+        mutation: {
+          background: 'https://i.pinimg.com/originals/66/f8/59/66f859fc32c72e5f8401cc03e09ebc18.png',
+        },
+        children: [
+          {
+            text: 'I would rather talk about trees.',
+            mutation: {
+              title: 'Tree Chat',
+              background: 'http://wallpaperose.com/wp-content/uploads/2013/07/Natural-Leaves-Widescreen.jpg',
+            },
+            children: [
+              {
+                text: 'Okay. Lets talk about trees, then.',
+              },
+              {
+                text: 'No. Trees are stupid.',
+              },
+            ],
+          },
+        ],
+      },
+    ],
   };
-
-  useEffect(() => {
-    setDepth(path.length);
-  }, [path.length]);
 
   return (
     <div
@@ -60,47 +105,7 @@ export const Index = () => {
 
       <h1>{coolState.title}</h1>
 
-      <ContextStacker
-        addPath={addPath}
-        setDepth={setDepthOk}
-        path={path.slice(0, depth)}
-        text='Hello.'
-        depthLimit={depth}
-        ancestorProps={{
-          title: 'Welcome!',
-        }}
-        callback={setCoolState}
-      >
-        <ContextStacker text='Blah blah blah, but, in cyan' color='cyan' />
-        <ContextStacker text='I like camping.' mutation={{ background: 'http://jpkramer.com/fire2.gif' }} />
-        <ContextStacker
-          text='Who wants to talk spaceships?'
-          mutation={{
-            title: 'Spaceship Talk!',
-            background: 'https://blogs.esa.int/alexander-gerst/files/2014/03/10729802334_f51e9b69d8_o.jpg',
-          }}
-        />
-        <ContextStacker text='Britta, are you here?' />
-        <ContextStacker
-          text='Hey look I can change the background.'
-          color='yellow'
-          changeAncestorProps={(props) => ({
-            ...props,
-            background: 'https://i.pinimg.com/originals/66/f8/59/66f859fc32c72e5f8401cc03e09ebc18.png',
-          })}
-        >
-          <ContextStacker
-            text='I would rather talk about trees.'
-            mutation={{
-              title: 'Tree Chat',
-              background: 'http://wallpaperose.com/wp-content/uploads/2013/07/Natural-Leaves-Widescreen.jpg',
-            }}
-          >
-            <ContextStacker text='Okay. Lets talk about trees, then.' />
-            <ContextStacker text='No. Trees are stupid.' />
-          </ContextStacker>
-        </ContextStacker>
-      </ContextStacker>
+      {stack(data)}
 
       <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
         <BottomNavigation showLabels>
@@ -111,4 +116,9 @@ export const Index = () => {
       </Paper>
     </div>
   );
+};
+
+const stack = (data) => {
+  const { children, ...props } = data;
+  return <ContextStacker {...props}>{children && children.map((child) => stack(child))}</ContextStacker>;
 };
