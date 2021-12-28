f420b88e3adeacb66e47f83b3850ce16b7b56ad2
Joel Kramer <joel@nuffsaid.com>
2021-12-27 19:53:01 -0800

    ...

--- src/client/lib/redux/xselectors.test.ts
+++ src/client/lib/redux/xselectors.test.ts
@@ -12,59 +12,60 @@ import { client } from '../../../shared/lib/redux/actionTypes';
 import { inspect } from 'util';
 
 const query = (query) => store.getState().nodes.filter(sift(query));
+const createMessage = (text, rel = {}) => ({
+  _id: new mongoose.Types.ObjectId(),
+  kind: 'Message',
+  text,
+  rel,
+});
 
-let baseNode, downstreamNode;
+let blackElk, foolsCrow, wakanTanka;
 beforeAll(() => {
-  baseNode = {
-    _id: new mongoose.Types.ObjectId(),
-    kind: 'Message',
-    text: 'Fools Crow',
-  };
-  downstreamNode = {
-    _id: new mongoose.Types.ObjectId(),
-    kind: 'Message',
-    text: 'Child of Fools Crow',
-    rel: {
-      upstreams: [baseNode._id],
-    },
-  };
+  wakanTanka = createMessage('Wakan Tanka');
+  blackElk = createMessage('Black Elk', { authors: [wakanTanka._id] });
+  foolsCrow = createMessage('Fools Crow', {
+    upstreams: [blackElk._id],
+    authors: [wakanTanka._id],
+  });
+
   store.dispatch({
     type: client.STASH,
-    payload: [baseNode, downstreamNode],
+    payload: [blackElk, foolsCrow, wakanTanka],
   });
 });
 
 test('Setup succeeded', () => {
   const nodes = store.getState().nodes;
-  expect(nodes).toHaveLength(2);
-  expect(nodes[0].text).toBe(baseNode.text);
+  expect(nodes).toHaveLength(3);
+  expect(nodes[0].text).toBe(blackElk.text);
 });
 
 test('Can get by ID', () => {
-  const [node] = query(idQuery(baseNode._id));
-  expect(node.text).toBe(baseNode.text);
+  const [node] = query(idQuery(blackElk._id));
+  expect(node.text).toBe(blackElk.text);
 });
 
 test('Can get by lacked relation', () => {
   const [node] = query(lacksRelationQuery('upstreams'));
-  expect(node.text).toBe(baseNode.text);
+  expect(node.text).toBe(blackElk.text);
 });
 
 test('Can get by has relation', () => {
   const q = hasRelationQuery('upstreams');
   // console.log(inspect(q, { depth: null }));
   const [node] = query(q);
-  expect(node.text).toBe(downstreamNode.text);
+  expect(node.text).toBe(foolsCrow.text);
 });
 
 test('Can get relations of a given node', () => {
-  const q = relationsOfQuery(baseNode._id, 'upstreams', 'authors');
+  const q = relationsOfQuery(blackElk._id, 'upstreams', 'authors');
   const nodes = query(q);
-  expect(nodes[0].text).toBe(downstreamNode.text);
+  expect(nodes).toHaveLength(1);
+  expect(nodes[0].text).toBe(foolsCrow.text);
 });
 
 test('Can run a profile', () => {
-  const profile = ['id', downstreamNode._id];
+  const profile = ['id', foolsCrow._id];
   const [node] = query(runProfile(profile));
-  expect(node.text).toBe(downstreamNode.text);
+  expect(node.text).toBe(foolsCrow.text);
 });
